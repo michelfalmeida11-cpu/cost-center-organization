64c40ae259301ebe3bba98575876abd36a82ce4a
michelfalmeida11-cpu <michelfalmeida11@gmail.com>
2026-05-03 21:57:01 -0300

    commit

--- next.config.js
+++ next.config.js
@@ -1,8 +1,4 @@
-/**
- * @type {import('next').NextConfig}
- */
-const nextConfig = {
-  /* config options here */
-}
- 
+/** @type {import('next').NextConfig} */
+const nextConfig = {}
+
 module.exports = nextConfig
