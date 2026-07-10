70677097d80705bd53e3cffdeec31c823c9ab04f
michelfalmeida11-cpu <michelfalmeida11@gmail.com>
2026-07-10 14:59:52 -0300

    commit

--- app/administracao/page.tsx
+++ app/administracao/page.tsx
@@ -1,7 +1,8 @@
-export default function Home() {
-  return (
-    <h1 style={{ color: "red", fontSize: "60px" }}>
-      TESTE VERCEL 999999
-    </h1>
-  );
-}
+import {
+  CurrencyDollar,
+  GasPump,
+  Truck,
+  ShieldCheck,
+  CraneTower,
+  Hammer,
+} from "@phosphor-icons/react";
